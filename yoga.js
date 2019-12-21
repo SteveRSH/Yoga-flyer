48a538b8bde979da7fabed09ea19fc528b8e7fd2
Steve Horlback <stevehorlback@steves-mbp.attlocal.net>
2019-12-21 08:47:17 -0500

    poster

---
+++ yoga.js
@@ -0,0 +1,20 @@
+document.getElementById("yoga_heading1").innerHTML = "yoga";
+document.getElementById("yoga_heading2").innerHTML = "classes";
+
+document.getElementById("availableSchedule").innerHTML = "Available Tuesdays 6-7 PM";
+document.getElementById("scheduleAvailable").innerHTML = "&#38; Saturdays 6-7 AM";
+
+
+//CSS
+
+document.getElementById("yoga_heading1").style.letterSpacing = "15px";
+document.getElementById("yoga_heading2").style.letterSpacing = "15px";
+document.getElementById("availableSchedule").style.letterSpacing = "10px";
+document.getElementById("scheduleAvailable").style.letterSpacing = "10px";
+document.getElementById("firstParagraph").style.letterSpacing = "10px";
+
+
+document.getElementById("yoga_heading1").style.color = "#FFDA9F";
+document.getElementById("yoga_heading2").style.color = "#FFDA9F";
+document.getElementById("availableSchedule").style.color = "#FFF1CD";
+document.getElementById("scheduleAvailable").style.color = "#FFF1CD";
